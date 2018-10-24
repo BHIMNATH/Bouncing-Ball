ab938799ec7cf9e51e82cd5697fdbe54c98578a6
Aswin Bhim Nath <aswismail@gmail.com>
2018-10-24 09:50:55 +0530

    Title Function

--- main.lua
+++ main.lua
@@ -6,3 +6,9 @@
 -- Your code here 
 
 function createTitleScreen()
+-- local titlescreen = display.newImage("titlescreen.png")
+	local startgametext = display.newText("Click here to start", 0 , 0, "Helvetica" , 24 )
+
+	startgametext.x = display.contentCenterX
+	startgametext.y = display.contentCenterY
+	startgametext:setTextColor(0,22,22)
