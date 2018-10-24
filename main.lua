08c576da9d6cdd0136a5c15d6ce354c7bb9c3a6a
Aswin Bhim Nath <aswismail@gmail.com>
2018-10-24 09:51:11 +0530

    Title Function

--- main.lua
+++ main.lua
@@ -12,3 +12,14 @@ function createTitleScreen()
 	startgametext.x = display.contentCenterX
 	startgametext.y = display.contentCenterY
 	startgametext:setTextColor(0,22,22)
+	
+	local function startPressed(event)
+		display.remove(event.target)
+		-- display.remove(titlescreen)
+		startgametext = nil
+		createGame()
+ 
+	end
+ 
+        startgametext:addEventListener ( "tap", startPressed )
+end
